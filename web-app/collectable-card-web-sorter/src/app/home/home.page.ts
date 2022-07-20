70a458b44368fb7a58d5aed2f4fb0d01de480fdf
Zac <poisonite@gmail.com>
2022-07-20 01:00:50 -0700

    Dummy login and nav
    Built basic dummy login on home page (not hooked up but full form etc)
    "authed" home view shows nav leading to each of our initial pages

--- web-app/collectable-card-web-sorter/src/app/home/home.page.ts
+++ web-app/collectable-card-web-sorter/src/app/home/home.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+// Core+
+import { Component, OnInit } from '@angular/core';
+
+// Angular Reactive Forms
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  isAuth = false;
 
-  constructor() {}
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
+  signIn() {
+    // const email = this.loginForm.controls.email.value;
+    // const password = this.loginForm.controls.password.value;
+    // this.authService.SignIn(email, password);
+    this.loginForm.reset();
+
+    // UI Demo Only
+    this.isAuth = !this.isAuth;
+  }
+
+  ngOnInit(): void {
+    // Form config and validation for the Add User Form
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('test@email.com', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl('password1234', [Validators.required]),
+    });
+  }
 }
