7325c37e474c5dcc5d643665ef3894df0095155d
Zac <poisonite@gmail.com>
2022-07-20 00:11:38 -0700

    Initial Blank Web App - V0.0.1
    Blank ionic 6 & angular 14 app, no other installations etc

---
+++ web-app/collectable-card-web-sorter/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'collectable-card-web-sorter',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
